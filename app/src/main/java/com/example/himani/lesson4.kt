bf4866a53131d4c2b229572fc0d24175cf93454d
himanikunwar03 <himanikunwar098@gmail.com>
2025-04-21 19:32:03 +0545

    lesson5

--- app/src/main/java/com/example/himani/lesson4.kt
+++ app/src/main/java/com/example/himani/lesson4.kt
@@ -64,6 +64,18 @@ fun main (){
 
 
 
+    var a: Int = 5
+    var b: Int = 5
+
+//    println("a == b : " + (a == b))
+//    println("a != b : " + (a != b))
+//    println("a < b : " + (a < b))
+//    println("a > b : " + (a > b))
+//    println("a >= b : " + (a >= b))
+//    println("a <= b : " + (a <= b))
+
+
+
 
 
 
